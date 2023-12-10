51646c3964e1134f29df6ca25211a9fc2e050f51
Michael Nguyen <aqmnguyen@gmail.com>
2023-12-10 15:49:32 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { getMimeType, getMimeExtension } from '@mn-test/sample-mime-lib';
+import { isOddNumber } from '@mn-test/sample-mime-lib';
 
-export const useMimeType = (filename: string) => {
-  return `Your mime type is ${getMimeType(filename)}`;
-};
-
-export const useMimeExtension = (mimeType: string) => {
-  return `Your mime extension is ${getMimeExtension(mimeType)}`;
+export const useIsOdd = (num: number | string) => {
+  return `Your value of ${num} has returned back ${isOddNumber(
+    num
+  )} for being odd`;
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,14 @@
-import { useMimeType, useMimeExtension } from '../src';
+import { useIsOdd } from '../src';
 
-describe('useMimeType', () => {
-  it('should return the mime type', () => {
-    expect(useMimeType('test.txt')).toBe('Your mime type is text/plain');
+describe('useIsOdd', () => {
+  it('should return the odd number', () => {
+    expect(useIsOdd(3)).toBe(
+      'Your value of 3 has returned back true for being odd'
+    );
   });
-});
-
-describe('useMimeExtension', () => {
-  it('should return the mime extension', () => {
-    expect(useMimeExtension('text/plain')).toBe('Your mime extension is txt');
+  it('should return the even number', () => {
+    expect(useIsOdd(4)).toBe(
+      'Your value of 4 has returned back false for being odd'
+    );
   });
 });
